6b832687cc9a9a9a559609930cccdac04f21f50b
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-05-04 10:32:24 +0700

    show picker when last item on adapter clicked

--- app/src/main/java/id/bungamungil/imagepickerdemo/MainActivity.kt
+++ app/src/main/java/id/bungamungil/imagepickerdemo/MainActivity.kt
@@ -1,13 +1,17 @@
 package id.bungamungil.imagepickerdemo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ImageAdapter.OnItemViewClicked {
 
-    private val adapter = ImageAdapter()
+    private val adapter = ImageAdapter(this)
+
+    private val imagePickerRequest = 42
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,13 @@ class MainActivity : AppCompatActivity() {
     private fun setupRecyclerView() {
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.adapter = adapter
+    }
+
+    override fun onItemViewClicked(position: Int, itemView: View) {
+        if (position == adapter.itemCount - 1) {
+            val imagePicker = Intent(Intent.ACTION_PICK)
+            imagePicker.type = "image/*"
+            startActivityForResult(imagePicker, imagePickerRequest)
+        }
     }
 }

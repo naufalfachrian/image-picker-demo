0588ae78e1e18166648318ef29e3f4e34a2cc309
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-05-04 10:57:32 +0700

    list of uri and display image from uri

--- app/src/main/java/id/bungamungil/imagepickerdemo/ImageAdapter.kt
+++ app/src/main/java/id/bungamungil/imagepickerdemo/ImageAdapter.kt
@@ -1,13 +1,17 @@
 package id.bungamungil.imagepickerdemo
 
+import android.net.Uri
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_image.view.*
 
 class ImageAdapter(private val onItemViewClicked: OnItemViewClicked? = null) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    val items = ArrayList<Uri>()
 
     override fun getItemViewType(position: Int): Int {
         return R.layout.item_image
@@ -19,12 +23,16 @@ class ImageAdapter(private val onItemViewClicked: OnItemViewClicked? = null) : R
     }
 
     override fun getItemCount(): Int {
-        return 3
+        return items.count() + 1
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.itemView.image.setImageDrawable(ContextCompat.getDrawable(holder.itemView.context, R.drawable.ic_add_circle_outline_black_24dp))
-
+        val url = items.getOrNull(position)
+        if (url != null) {
+            Picasso.get().load(url).resize(120, 120).centerCrop().into(holder.itemView.image)
+        } else {
+            holder.itemView.image.setImageDrawable(ContextCompat.getDrawable(holder.itemView.context, R.drawable.ic_add_circle_outline_black_24dp))
+        }
         holder.itemView.setOnClickListener {
             onItemViewClicked?.onItemViewClicked(position, it)
         }

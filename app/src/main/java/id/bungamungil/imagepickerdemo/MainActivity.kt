971a126c2510928e7e6f68ed8b640b4dac5726f9
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-05-04 10:20:58 +0700

    setup main activity

--- app/src/main/java/id/bungamungil/imagepickerdemo/MainActivity.kt
+++ app/src/main/java/id/bungamungil/imagepickerdemo/MainActivity.kt
@@ -2,11 +2,21 @@ package id.bungamungil.imagepickerdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = ImageAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
     }
 }
